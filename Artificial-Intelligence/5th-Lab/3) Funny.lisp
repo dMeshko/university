d19565ae1638094649e270065ac5079551669ad2
dMeshko <dMeshko@users.noreply.github.com>
2015-04-30 23:52:23 +0200

    Create 3) Funny.lisp

---
+++ Artificial-Intelligence/5th-Lab/3) Funny.lisp
@@ -0,0 +1,31 @@
+; read input
+
+(defun read-input(p)
+(cond
+	((null p) nil)
+    (T (cons p (read-input (read nil nil))))
+)
+)
+
+(defun readinput(i)
+	(cdr (read-input 1))
+)  
+;--don't change above this line
+
+
+; write your function here
+(defun funny(n)
+(
+COND
+ ((= N 1) 1)
+ ((= (MOD N 2) 0) (* 2 (FUNNY (- N 1))))
+ ((AND (/= (MOD N 2) 0) (> N 1)) (+ (FUNNY (- N 1)) (FUNNY (- N 2))))
+ (T (NIL))
+)
+)
+
+
+
+
+;--don't change below this line
+(print (funny (car (readinput nil))))
