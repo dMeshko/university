c9e192e219a2715e6dbad1ff0cd0f754e46ab2d9
dMeshko <dMeshko@users.noreply.github.com>
2015-05-27 18:34:33 +0200

    Update Depth First Search with knights.lisp

--- Artificial-Intelligence/6th-Lab/Depth First Search with knights.lisp
+++ Artificial-Intelligence/6th-Lab/Depth First Search with knights.lisp
@@ -23,5 +23,75 @@
 (defun run-depth (start goal moves)
   (depth-first-search start goal () moves))
 
+(DEFUN UP-LEFT (POSITION)
+	(LET ((X (- (CAR POSITION) 2)) (Y (+ (CADR POSITION) 1))) 
+	(COND
+		((SAFE X Y) (LIST X Y))
+		(T NIL)
+	)) 
+)
+
+(DEFUN UP-LEFTT (POSITION)
+	(LET ((X (- (CAR POSITION) 1)) (Y (+ (CADR POSITION) 2))) 
+	(COND
+		((SAFE X Y) (LIST X Y))
+		(T NIL)
+	)) 
+)
+
+(DEFUN UP-RIGHT (POSITION)
+	(LET ((X (+ (CAR POSITION) 2)) (Y (+ (CADR POSITION) 1))) 
+	(COND
+		((SAFE X Y) (LIST X Y))
+		(T NIL)
+	)) 
+)
+
+(DEFUN UP-RIGHTT (POSITION)
+	(LET ((X (+ (CAR POSITION) 1)) (Y (+ (CADR POSITION) 2))) 
+		(COND
+		((SAFE X Y) (LIST X Y))
+		(T NIL)
+	)) 
+)
+
+(DEFUN DOWN-LEFT (POSITION)
+	(LET ((X (- (CAR POSITION) 2)) (Y (- (CADR POSITION) 1))) 
+	(COND
+		((SAFE X Y) (LIST X Y))
+		(T NIL)
+	)) 
+)
+
+(DEFUN DOWN-LEFTT (POSITION)
+	(LET ((X (- (CAR POSITION) 1)) (Y (- (CADR POSITION) 2))) 
+	(COND
+		((SAFE X Y) (LIST X Y))
+		(T NIL)
+	)) 
+)
+
+(DEFUN DOWN-RIGHT (POSITION)
+	(LET ((X (+ (CAR POSITION) 2)) (Y (- (CADR POSITION) 1))) 
+	(COND
+		((SAFE X Y) (LIST X Y))
+		(T NIL)
+	)) 
+)
+
+(DEFUN DOWN-RIGHTT (POSITION)
+	(LET ((X (+ (CAR POSITION) 1)) (Y (- (CADR POSITION) 2))) 
+	(COND
+		((SAFE X Y) (LIST X Y))
+		(T NIL)
+	)) 
+)
+
+(DEFUN SAFE (X Y)
+	(AND (> X 0) (> Y 0) (< X 4) (< Y 4))
+)
+
+(SETQ MOVES '(UP-LEFT UP-LEFTT UP-RIGHT UP-RIGHTT DOWN-LEFT DOWN-LEFTT DOWN-RIGHT DOWN-RIGHTT))
+
 ; add the moves list and uncomment the following line:
-;(print (run-depth '(1 1) '(3 3) ))
+(print (run-depth '(1 1) '(3 3) MOVES))
